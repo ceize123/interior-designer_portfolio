45ebbd52998919b1bc19e9ed5a13c295e85977b2
ytkuo <hahaeating1@gmail.com>
2023-01-16 17:47:36 -0500

    1/16 add html structure

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,93 @@
 import Image from 'next/image'
 
 export default function Home() {
-  return (
-    <>
-      <main className="text-emerald-600 font-bold">
-		<p>333</p>
-		<p>333</p>
-      </main>
-    </>
-  )
+	return (
+	<>
+		<section>
+			<h1>INTERIOR.FURNITURE</h1>
+			<h3>My WORK</h3>
+			<span>Effectively bring artistically and business professional solutions to clients.</span>	
+			<span>Worked on individual projects and as well as collaborated with others as a team</span>	
+		</section>
+		<main>
+			<section>
+				<h4>
+					I focus on using effectively optimize the space to improving people's everyday lives in all aspects.
+				</h4>
+				<h4>
+					Use the power of interior design to evokes happiness and soothes the soul that help people live better.	
+				</h4>
+			</section>	
+			<section>
+				<div>
+					{/* <img />	 */}
+					<div>
+						<p>(437) 97101353</p>
+						<p>ann1212123@gmail.com</p>
+						<p>Toronto,ON</p>
+					</div>
+				</div>
+				<div>
+					<div>
+						<h2>EDU.</h2>
+						<div>
+							<div>
+								<h4></h4>
+								<p></p>
+								<p></p>
+							</div>	
+							<div>
+								<h4></h4>
+								<p></p>
+								<p></p>
+							</div>	
+						</div>
+					</div>
+					<div>
+						<h2>SKILLS</h2>
+						<ul>
+							<li></li>
+							<li></li>
+							<li></li>
+							<li></li>
+						</ul>	
+					</div>
+					<div>
+						<h2>LANG.</h2>
+						<ul>
+							<li></li>
+							<li></li>
+						</ul>		
+					</div>
+					<div>
+						<h2>EXPERIENCE</h2>
+						<div>
+							<div>
+								<h4></h4>	
+								<p></p>	
+								<ul>
+									<li></li>
+									<li></li>
+									<li></li>
+								</ul>	
+							</div>
+							<div>
+								<h4></h4>	
+								<p></p>	
+								<ul>
+									<li></li>
+									<li></li>
+									<li></li>
+								</ul>	
+							</div>
+						</div>	
+					</div>
+				</div>
+			</section>
+		</main>
+		<footer>
+			<p>All work © Jhiih-Yuan (Ann) Tseng 2022.  Please do not distribute images without permission.</p>
+		</footer>
+	</>
+	)
 }
