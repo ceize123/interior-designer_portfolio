41bd64f2d9e24c93da2c01dc2500b8bec0e878ad
ytkuo <hahaeating1@gmail.com>
2023-01-20 16:32:04 -0500

    remove eslint

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import Image from 'next/image'
 import rabbit from '../public/rabbit.png'
 import { MdPhoneIphone, MdEmail, MdLocationOn } from 'react-icons/Md';
