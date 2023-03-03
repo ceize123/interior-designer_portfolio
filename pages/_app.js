e00a12979570c0e12fc502142024fdcebc5987a0
ytkuo <hahaeating1@gmail.com>
2023-03-03 16:33:40 -0500

    add blur for image loading

--- pages/_app.js
+++ pages/_app.js
@@ -5,16 +5,20 @@ import Contact from '../components/Contact'
 import { useRouter } from 'next/router'
 
 export default function App({ Component, pageProps }) {
-	const router = useRouter()
+  const router = useRouter()
 
-	return (
-		<div className={`layout w-full ${router.route === '/' ? 'bg-light-gray text-white' : ''}`}>
-			<Navbar />
-			<div className='container mx-auto'>
-				<Component {...pageProps} />
-			</div>
-			{router.route !== '/' && <Contact />}
-			<Footer />
-		</div>
-	)
+  return (
+    <div
+      className={`layout w-full ${
+        router.route === '/' ? 'bg-light-gray text-white' : ''
+      }`}
+    >
+      <Navbar />
+      <div className='container mx-auto'>
+        <Component {...pageProps} />
+      </div>
+      {router.route !== '/' && <Contact />}
+      <Footer />
+    </div>
+  )
 }
--- pages/works/[slug].js
+++ pages/works/[slug].js
@@ -7,117 +7,122 @@ import AOS from 'aos'
 import 'aos/dist/aos.css'
 
 const client = createClient({
-	space: process.env.CONTENTFUL_SPACE_ID, // id
-	accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+  space: process.env.CONTENTFUL_SPACE_ID, // id
+  accessToken: process.env.CONTENTFUL_ACCESS_KEY,
 })
 
 // after nextjs runs this, it generates different paths and pages
 export const getStaticPaths = async () => {
-	const res = await client.getEntries({
-		content_type: 'annWorks'
-	})
+  const res = await client.getEntries({
+    content_type: 'annWorks',
+  })
 
-	const paths = res.items.map((item) => {
-		return {
-			params: {slug: item.fields.slug}
-		}
-	})
+  const paths = res.items.map((item) => {
+    return {
+      params: { slug: item.fields.slug },
+    }
+  })
 
-	return {
-		paths,
-		fallback: true
-	}
+  return {
+    paths,
+    fallback: true,
+  }
 }
 
 // for each one of different paths, it runs getStaticProps
-export async function getStaticProps({params}) {
-	const { items } = await client.getEntries({
-		content_type: 'annWorks',
-		'fields.slug': params.slug
-	})
+export async function getStaticProps({ params }) {
+  const { items } = await client.getEntries({
+    content_type: 'annWorks',
+    'fields.slug': params.slug,
+  })
 
-	if (!items.length) {
-		return {
-			redirect: {
-				destination: '/',
-				permanent: false // not a permanent redirect, because in the future, there might have this url
-			}
-		}
-	}
+  if (!items.length) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false, // not a permanent redirect, because in the future, there might have this url
+      },
+    }
+  }
 
-	return {
-		props: { work: items[0] },
-		revalidate: 10
-	}
+  return {
+    props: { work: items[0] },
+    revalidate: 10,
+  }
 }
 
 export default function WorkDetails({ work }) {
-	useEffect(() => {
-		setTimeout(() => {
-			AOS.init({
-				offset: 150,
-				duration : 1000
-			})
-		}, 1500)
-	}, [work])
-	if (!work) return <Skeleton />
+  useEffect(() => {
+    setTimeout(() => {
+      AOS.init({
+        offset: 150,
+        duration: 1000,
+      })
+    }, 1500)
+  }, [work])
+  if (!work) return <Skeleton />
 
-	const { title, banner, year, overview, sketches } = work.fields
-	const sketchDrawings = sketches.slice(1)
-	let secAry = []
+  const { title, banner, year, overview, sketches } = work.fields
+  const sketchDrawings = sketches.slice(1)
+  let secAry = []
 
-	const getSections = (data) => {
-		const ary = Object.keys(data)
-		for (const item of ary) {
-			if (/sec.*Drawing/i.test(item)) {
-				secAry.push(data[item])
-			}
-		}
-	}
-	getSections(work.fields)
+  const getSections = (data) => {
+    const ary = Object.keys(data)
+    for (const item of ary) {
+      if (/sec.*Drawing/i.test(item)) {
+        secAry.push(data[item])
+      }
+    }
+  }
+  getSections(work.fields)
 
-	return (
-	<main className='px-14'>
-		<section className='banner flex justify-center md:py-24 py-16 min-h-screen'>
-			<div className='max-w-4xl w-full' data-aos='fade-up'>
-				<div>
-					<ImageTemp data={banner} />
-				</div>	
-				<div className='ml-5 mb-6'>
-					<h3 className='text-3xl mt-5'>{title}</h3>
-					<h4 className='text-1xl mt-2 text-light-gray'>{year}</h4>
-				</div>
-				<p className='font-bold leading-8'>{overview}</p>	
-			</div>	
-		</section>
-		<section className='md:py-20 py-12 min-h-screen'>
-			<div className='md:grid md:grid-cols-8 md:gap-2 flex flex-col' data-aos='fade-up'>
-				<div className='md:col-span-5'>
-					<div>
-						<ImageTemp data={sketches[0]} />
-					</div>	
-					<p className='font-bold leading-8 md:w-2/3'>{sketches[0].fields.description}</p>	
-				</div>
-				<div className='lg:col-span-2 md:col-span-3 md:self-center flex justify-center md:block'>
-					{sketchDrawings.map((item, idx) => {
-						return (
-							<div key={idx} className='md:w-full w-1/3'>
-								<ImageTemp data={item} />
-							</div>
-						)
-					})}
-				</div>	
-			</div>	
-		</section>
-		<section className='md:pb-20 pb-12'>
-			{secAry.map((item, idx) => {
-				return (
-					<div key={idx} className='mt-20 md:pb-20 pb-8' data-aos='fade-up'>
-						<Drawing ary={item} aryIdx={idx} />
-					</div>
-				)	
-			})}	
-		</section>	
-	</main>
-	)
+  return (
+    <main className='px-14'>
+      <section className='banner flex justify-center md:py-24 py-16 min-h-screen'>
+        <div className='max-w-4xl w-full'>
+          <div>
+            <ImageTemp data={banner} />
+          </div>
+          <div className='ml-5 mb-6'>
+            <h3 className='text-3xl mt-5'>{title}</h3>
+            <h4 className='text-1xl mt-2 text-light-gray'>{year}</h4>
+          </div>
+          <p className='font-bold leading-8'>{overview}</p>
+        </div>
+      </section>
+      <section className='md:py-20 py-12 min-h-screen'>
+        <div
+          className='md:grid md:grid-cols-8 md:gap-2 flex flex-col'
+          data-aos='fade-up'
+        >
+          <div className='md:col-span-5'>
+            <div>
+              <ImageTemp data={sketches[0]} />
+            </div>
+            <p className='font-bold leading-8 md:w-2/3'>
+              {sketches[0].fields.description}
+            </p>
+          </div>
+          <div className='lg:col-span-2 md:col-span-3 md:self-center flex justify-center md:block'>
+            {sketchDrawings.map((item, idx) => {
+              return (
+                <div key={idx} className='md:w-full w-1/3'>
+                  <ImageTemp data={item} />
+                </div>
+              )
+            })}
+          </div>
+        </div>
+      </section>
+      <section className='md:pb-20 pb-12'>
+        {secAry.map((item, idx) => {
+          return (
+            <div key={idx} className='mt-20 md:pb-20 pb-8' data-aos='fade-up'>
+              <Drawing ary={item} aryIdx={idx} />
+            </div>
+          )
+        })}
+      </section>
+    </main>
+  )
 }
